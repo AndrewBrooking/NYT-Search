a25d029f6c9da7ae20fca7645fb5f68307fbf81a
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-23 21:04:57 -0500

    Intial upload with front-end complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+const apiKey = "FTzJT8CD2KGGrRzGj2G7RK8UNpDq8A3h";
+const nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+$(document).ready(function() {
+
+});
+
+function runQuery(phrase) {
+    // TODO
+}
